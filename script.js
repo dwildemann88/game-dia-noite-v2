191e3c7a8f8a2a50942f86ad2c2bc00ecf3a0300
dwildemann88 <145924257+dwildemann88@users.noreply.github.com>
2024-08-28 14:54:11 -0300

    ajuste de botoes e animacoes

--- script.js
+++ script.js
@@ -9,22 +9,44 @@ function onloadSun (){
     tela.classList.add("dia")
     grama.style.filter='brightness(1.2)'
 }
+function alignObject(){
+    objeto.classList.add("alignObject")
+}
+function removeAnimation (){
+    objeto.classList.remove("animacao")
+    alignObject()
+    
+}
 botoes.forEach(botao => {
     botao.addEventListener("click", () => {
         
         let valor = botao.value;
-        objeto.classList.remove("sol", "lua","animacao");
-
+        objeto.classList.remove("sol", "lua");
         tela.classList.remove("dia", "noite");
-
+        setTimeout(
+            removeAnimation,1000
+        )
         if(valor==="lua"){
             objeto.classList.add("lua","animacao")
             tela.classList.add("noite")
             grama.style.filter='brightness(0.7)'
+            
+            
         }else{
             objeto.classList.add("sol","animacao")
             tela.classList.add("dia")
             grama.style.filter='brightness(1.2)'
+            
         }
+        
+        
     });
 });
+
+document.querySelectorAll('.botao').forEach(button => {
+    button.addEventListener('click', function() {
+        document.querySelectorAll('.botao').forEach(btn => btn.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
+
